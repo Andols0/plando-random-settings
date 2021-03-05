540a08a74e3dd1946b5168e4d7e441f4b5081367
Andols <github@andols.se>
2021-03-05 19:33:55 +0100

    Add conditional for max number of triforce pieces

--- Conditionals.py
+++ Conditionals.py
@@ -28,3 +28,30 @@ def include_40_50_skulls(random_settings):
     if random_settings['tokensanity'] in ['off']:
         random_settings['disabled_locations'].extend(["Kak 40 Gold Skulltula Reward",
         "Kak 50 Gold Skulltula Reward"])
+
+def max_triforce_pieces(random_settings):
+    if random_settings['triforce_hunt'] == "true":
+        if random_settings['item_pool_value'] == "balanced":
+            Max_pieces = 44
+            if random_settings["shuffle_cows"] == "true":
+                Max_pieces += 8
+            if random_settings["shuffle_scrubs"] != "off":
+                Max_pieces += 21
+            if random_settings["shopsanity"] != "off" and random_settings["shopsanity"] != "0" and random_settings["shopsanity"] != "random":
+                if random_settings["shopsanity"] == "1":
+                    Max_pieces += 3
+                elif random_settings["shopsanity"] == "2":
+                    Max_pieces += 8
+                elif random_settings["shopsanity"] == "3":
+                    Max_pieces += 14
+                elif random_settings["shopsanity"] == "4":
+                    Max_pieces += 19
+            if random_settings["shuffle_mapcompass"] == "remove" or random_settings["shuffle_mapcompass"] == "startwith":
+                Max_pieces += 14
+            if random_settings["shuffle_smallkeys"] == "remove":
+                Max_pieces += 29
+            if random_settings["shuffle_bosskeys"] == "remove":
+                Max_pieces += 4
+            if random_settings['triforce_goal_per_world'] > Max_pieces:
+                random_settings['triforce_goal_per_world'] = Max_pieces      
+        
